454d377da9e84105085ad9016ccbeb8339f8b937
Rohit Singh <kailash.jc.rs@gmail.com>
2022-11-21 23:56:08 +0530

    updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
                     <Flex justifyContent='space-evenly' h={['44vh', '50vh', '50vh', '50vh']} w='50%' direction='column'>
                         <Text as='b' fontSize={{ base: '1.4rem', lg: '1.8rem', md: '1rem', sm: '1rem' }} > Mytheresa</Text>
                         <Text fontSize={{ base: '1.1rem', lg: '1.2rem', md: '1.2rem', sm: '1.1rem' }}>Made a clone of the famous luxury fashion and designer shopping e-commerce website.</Text>
-                        <Text as='b' fontSize={{ base: '0.9rem', lg: '1.2rem', md: '1.2rem', sm: '1.1rem' }}>Tech stack- React | HTML | CSS | Chakra-UI</Text>
+                        <Text as='b' fontSize={{ base: '0.9rem', lg: '1.1rem', md: '1.1rem', sm: '1.1rem' }}>Tech stack- React | HTML | CSS | Chakra-UI</Text>
                         <Flex w={['68%', '70%', '63%', '63%']} justifyContent='space-between' alignItems='center'>
                             <Link target='_blank' href='https://mytheresabyrohit.netlify.app/'>
                                 <Button w={['4rem', '5rem', '6rem', '7rem']} fontSize={['0.8rem', '0.9rem', '1rem', '1rem']} bgColor='rgb(64, 112, 244)' colorScheme='blue' color='white'>
@@ -32,13 +32,34 @@ const Projects = () => {
 
                 <Flex w={['100%', '80%', '80%', '80%']} mt='1.3rem' justifyContent='center' alignItems='center' boxShadow='rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px'>
                     <Flex h='50vh' w='50%' justifyContent='center' alignItems='center' direction='column'>
+                        <Image w='90%' src='https://github.com/Guruprasad3n/humane-shelf-7123/raw/main/Frontend/hubspot/src/Assets/Home.png' />
+                    </Flex>
+
+                    <Flex justifyContent='space-evenly' h={['44vh', '50vh', '50vh', '50vh']} w='50%' direction='column'>
+                        <Text as='b' fontSize={{ base: '1.4rem', lg: '1.8rem', md: '1rem', sm: '1rem' }} >Hubspot</Text>
+                        <Text fontSize={{ base: '1.1rem', lg: '1.2rem', md: '1.2rem', sm: '1.1rem' }}>HubSpot is a Cloud-Based CRM, HubSpot is a Software platform which is designed to help company market and sell more effectively.</Text>
+                        <Text as='b' fontSize={{ base: '0.9rem', lg: '1.1rem', md: '1.1rem', sm: '1.1rem' }}>Tech stack- React | NodeJs | Express | MongoDB</Text>
+                        <Flex w={['68%', '70%', '63%', '63%']} justifyContent='space-between' alignItems='center'>
+                            <Link target='_blank' href='https://animated-narwhal-cecc2b.netlify.app/'>
+                                <Button w={['4rem', '5rem', '6rem', '7rem']} fontSize={['0.8rem', '0.9rem', '1rem', '1rem']} bgColor='rgb(64, 112, 244)' colorScheme='blue' color='white'>
+                                    Website</Button>
+                            </Link>
+                            <Link target='_blank' href='https://github.com/Guruprasad3n/humane-shelf-7123'>
+                                <Button w={['4rem', '5rem', '6rem', '7rem']} fontSize={['0.8rem', '0.9rem', '1rem', '1rem']} bgColor='rgb(64, 112, 244)' colorScheme='blue' color='white'>Github</Button>
+                            </Link>
+                        </Flex>
+                    </Flex>
+                </Flex>
+
+                <Flex w={['100%', '80%', '80%', '80%']} mt='1.3rem' justifyContent='center' alignItems='center' boxShadow='rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px'>
+                    <Flex h='50vh' w='50%' justifyContent='center' alignItems='center' direction='column'>
                         <Image w='90%' src='https://i.postimg.cc/767BYptn/Screenshot-231.png' />
                     </Flex>
 
                     <Flex justifyContent='space-evenly' h={['44vh', '50vh', '50vh', '50vh']} w='50%' direction='column'>
                         <Text as='b' fontSize={{ base: '1.4rem', lg: '1.8rem', md: '1rem', sm: '1rem' }} >Nature's Basket</Text>
                         <Text fontSize={{ base: '1.1rem', lg: '1.2rem', md: '1.2rem', sm: '1.1rem' }}>Nature’s Basket is India’s pioneering food destination present through physical retail stores, online portal and a mobile application.</Text>
-                        <Text as='b' fontSize={{ base: '0.9rem', lg: '1.2rem', md: '1.2rem', sm: '1.1rem' }}>Tech stack- HTML | CSS | JavaScript | DOM</Text>
+                        <Text as='b' fontSize={{ base: '0.9rem', lg: '1.1rem', md: '1.1rem', sm: '1.1rem' }}>Tech stack- HTML | CSS | JavaScript | DOM</Text>
                         <Flex w={['68%', '70%', '63%', '63%']} justifyContent='space-between' alignItems='center'>
                             <Link target='_blank' href='https://nature-sbasket.netlify.app/'>
                                 <Button w={['4rem', '5rem', '6rem', '7rem']} fontSize={['0.8rem', '0.9rem', '1rem', '1rem']} bgColor='rgb(64, 112, 244)' colorScheme='blue' color='white'>
