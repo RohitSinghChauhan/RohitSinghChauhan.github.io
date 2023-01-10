5a0640c2d12ce6daf0ed1085a88d21466ed0fbd9
Rohit Singh <kailash.jc.rs@gmail.com>
2023-01-10 10:46:19 +0530

    Changes in calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,30 +3,12 @@ import GitHubCalendar from 'react-github-calendar';
 import ReactTooltip from 'react-tooltip';
 import { Box } from '@chakra-ui/react'
 
-const selectLastHalfYear = contributions => {
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const shownMonths = 6;
-
-    return contributions.filter(day => {
-        const date = new Date(day.date);
-        const monthOfDay = date.getMonth();
-
-        return (
-            date.getFullYear() === currentYear &&
-            monthOfDay > currentMonth - shownMonths &&
-            monthOfDay <= currentMonth
-        );
-    });
-};
-
 const Calendar = () => {
     return (
         <Box>
             <GitHubCalendar
                 style={{ margin: 'auto' }}
                 username="rohitsinghchauhan"
-                transformData={selectLastHalfYear}
                 blockSize={20}
                 fontSize={15}
             >
